c0a4acd0ad7e6305b93622676bf5334fa095ae69
Advl27j03 <adindadwiy04@gmail.com>
2026-07-13 17:47:46 +0700

    Add Raster Image

--- WEB APP ADINDA/src/main.js
+++ WEB APP ADINDA/src/main.js
@@ -38,6 +38,7 @@ const map = new Map({
 //}
 
 map.on('load', () => {
+
 //LAYER POLYGON
 map.addSource('pulau', {
     type: "geojson",
@@ -72,6 +73,23 @@ map.addLayer({
         }
   });
 
+//LAYER RASTER 
+ map.addSource("soreang", {
+    type: "image",
+    url: "https://upload.wikimedia.org/wikipedia/commons/3/37/Peta_Kecamatan_Soreang.jpg",
+    coordinates: [
+      [79.16, -0.40], // top-left
+      [94.18, -1.66], // top-right
+      [94.65, -14.73], // bottom-right
+      [72.97, -13.74] // bottom-left
+    ]
+  });
+
+ map.addLayer({
+    id: "soreang-image",
+    type: "raster",
+    source: "soreang",
+  });
 
 
 });
